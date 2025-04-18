9708b7f33a4c3688b39f5d07daf8b1f049c44f4c
Егор Коваленко <kowalenko@bk.ru>
2025-04-19 00:48:36 +0500

    Готово(но без коментов)

--- praktica3/Program.cs
+++ praktica3/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace praktica3
+﻿using System.Threading.Channels;
+
+namespace praktica3
 {
     internal class Program
     {
@@ -8,8 +10,31 @@
             var clients = BankAccounts.Clients;
             while (true)
             {
-                Console.WriteLine("Выбирите действие:\n" +
-                "0. gregergherh" +
+                Console.WriteLine("Выбирите действие:\n1)Операции со счетом\n" +
+                    "2)Операции банка");
+                int a;
+                while (!int.TryParse(Console.ReadLine(), out a))
+                {
+                    Console.WriteLine("Ошибка ввода, попробуйте снова");
+                }
+                switch (a)
+                {
+                    case 1:
+                        Console.WriteLine("Выбирите действие:\n1)Пополнение счета\n2)Сянтие со счета");
+                        int c;
+                        while (!int.TryParse(Console.ReadLine(), out c))
+                        {
+                            Console.WriteLine("Ошибка ввода, попробуйте снова");
+                        }
+                        switch (c)
+                        {
+                            case 1: BankAccounts.AddingFundsToAccount();break;
+                                case 2: BankAccounts.WithdrawalFromTheAccount();break;
+                            default: Console.WriteLine("Ошибка ввода"); break;
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Выбирите действие:\n" +
                 "1. Создания клиента \n" +
                 "2. Создание счета для уже существующего клиента\n" +
                 "3. Вывод всех клиентов с данными по их счетам\n" +
@@ -18,25 +43,28 @@
                 "6. Вывод суммы на всех счетах всех клиентов\n" +
                 "7. Вывод отдельно только кредитных счетов\n" +
                 "8. Вывод отдельно только дебетовых счетов");
-                int a;
-                while (!int.TryParse(Console.ReadLine(), out a))
-                {
-                    Console.WriteLine("Ошибка ввода, попробуйте снова");
-                }
-                switch (a)
-                {
-                    case 0: BankAccounts.WithdrawalFromTheAccount();break;
-                    case 10:BankAccounts.AddingFundsToAccount();break;
-                    case 1: Bank.CreateClient(clients); break;
-                    case 2: Bank.CreateAccountOfClient(clients); break;
-                    case 3: Bank.PrintInfoOfClients(clients); break;
-                    case 4: Bank.PrintInfoOfAccountsOfClient(clients); break;
-                    case 5: Bank.PrintSummOfClientAccounts(clients); break;
-                    case 6: Bank.PrintSummOfClients(clients); break;
-                    case 7: Bank.PrintOnlyCreditAccounts(clients); break;
-                    case 8: Bank.PrintOnlyDebitAccounts(clients); break;
+                        int b;
+                        while (!int.TryParse(Console.ReadLine(), out b))
+                        {
+                            Console.WriteLine("Ошибка ввода, попробуйте снова");
+                        }
+
+                        switch (b)
+                        {
+                            case 1: Bank.CreateClient(clients); break;
+                            case 2: Bank.CreateAccountOfClient(clients); break;
+                            case 3: Bank.PrintInfoOfClients(clients); break;
+                            case 4: Bank.PrintInfoOfAccountsOfClient(clients); break;
+                            case 5: Bank.PrintSummOfClientAccounts(clients); break;
+                            case 6: Bank.PrintSummOfClients(clients); break;
+                            case 7: Bank.PrintOnlyCreditAccounts(clients); break;
+                            case 8: Bank.PrintOnlyDebitAccounts(clients); break;
+                            default: Console.WriteLine("Ошибка ввода"); break;
+                        }
+                        break;
                     default: Console.WriteLine("Ошибка ввода"); break;
                 }
+               
                 Console.ReadKey();
                 Console.Clear();
             }
